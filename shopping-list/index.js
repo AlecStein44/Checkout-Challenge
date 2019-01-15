41ca02bc35d5bf2940ee0687c69ecff78d0af3cc
AlecStein44 <45604055+AlecStein44@users.noreply.github.com>
2019-01-15 13:20:14 -0800

    Add files via upload

---
+++ shopping-list/index.js
@@ -0,0 +1,34 @@
+$(function() {
+    
+    $('li').addClass('items');
+
+    $('#js-shopping-list-form').submit(function(event) {
+        event.preventDefault();
+        const listItem = $(this).find('#shopping-list-entry').val();
+
+        $('#shopping-list-entry').val('');
+    
+    
+        $('.shopping-list').append(
+          `<li>
+            <span class="shopping-item">${listItem}</span>
+            <div class="shopping-item-controls">
+              <button class="shopping-item-toggle">
+                <span class="button-label">check</span>
+              </button>
+              <button class="shopping-item-delete">
+                <span class="button-label">delete</span>
+              </button>
+            </div>
+          </li>`);
+      });
+    
+  
+    $('.shopping-list').on('click', '.shopping-item-delete', function(event) {
+        $(this).closest('li').slideUp();
+      });
+
+      $('.shopping-list').on('click', '.shopping-item-toggle', function(event) {
+        $(this).closest('li').find('.shopping-item').toggleClass('shopping-item__checked');
+      });
+    });
